2fcf66941fcbe2f1f2461a330536bf679e1c8100
JoaoVFG <joao.vfg@hotmail.com.br>
2019-03-01 16:02:31 -0300

    Desenvolvido ate aula 59

---
+++ CAP02_EstruturasDeControle/Aula53_if2.js
@@ -0,0 +1,17 @@
+function teste1(num){
+    if (num > 7)
+        console.log(num)
+        console.log('final')
+}
+
+function teste2(num){
+    if (num > 7){
+        console.log(num)
+        console.log('final')
+    }
+}
+
+teste1(6)
+teste1(8)
+teste2(6)
+teste2(8)
