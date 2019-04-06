65742d779b9bfebae356b93acf7c97337f0c9f8f
João Vitor F Garcia <joao.vfg@hotmail.com.br>
2019-04-06 11:39:25 -0300

    Finalizado até aula 86

---
+++ CAP04_OrientacaoObjeto/Aula86_oo.js
@@ -0,0 +1,26 @@
+// CODIGO NAO EXECUTAVEL 
+
+//PROCEDURAL -> foco em funções que manipulam dados
+processamento(valor1,valor2,valor3)
+
+// OO -> voce tem dados e dentro desses dados tem funções
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento(){
+
+    }
+}
+
+objeto.processamento()
+// mapear o mundo com base em objeto e traze-los para dentro desse softwares
+
+/** principios importantes:
+ * 1 - abstração
+ * 2 - encapsulmento
+ * 3 - herança(prototype)
+ * - COMPOSIÇAO - PRIORIZAR
+ * 4 - Polimorfismo - multiplas formas
+ * */
